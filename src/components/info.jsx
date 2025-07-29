dbbbf6a2fb125f3a462dd1de590ca415f7450b92
Bruno <finhana.bruno@gmail.com>
2025-07-29 12:33:04 -0300

    Starting the props destructuring

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,12 +1,13 @@
 import ProfilePic from "../assets/profilePics/user1.jpg";
 
 export default function Header(props) {
+    const {...otherProps} = props;
     return(
         <div className="infoHolder">
             <div className="profilePicHolder">
             </div>
-            <h1>{props.userName}</h1>
-            <h2>{props.occupation}</h2>
+            <h1>{userName}</h1>
+            <h2>{occupation}</h2>
             <div>
                 <button>Email</button>
                 <button>LinkedIn</button>
